b8d2e18b828ce0116a5eaa80c705bc8b6e321f65
Ryan Lee <ryanlee619@gmail.com>
2023-10-27 23:36:59 -0700

    uploadede firebase and function in items to get text from mpe from openai api

--- backend/app/dependencies/items.py
+++ backend/app/dependencies/items.py
@@ -1,1 +1,16 @@
 """Dependencies for items endpoints."""
+import openai
+from dotenv import load_dotenv
+import os
+import asyncio
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+async def audio_to_text():
+    audio_file= open("/Users/ryanlee/Desktop/Repos/aurora/backend/test_files/test_audio.mp3", "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    return transcript['text']
+
+if __name__ == "__main__":
+    print(audio_to_text())
