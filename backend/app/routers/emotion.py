cbf6ffd0d8f8410d9b0d4a5db4034718eb794e86
Andrew Hinh <ajhinh@gmail.com>
2023-10-28 00:38:57 -0700

    Added firebase integration + move to Hume only

--- backend/app/routers/emotion.py
+++ backend/app/routers/emotion.py
@@ -1,13 +1,12 @@
 """Feature routes."""
 from fastapi import APIRouter
 
-from ..dependencies.emotion import get_emotions
+from ..dependencies.emotion import get_emotion_data
 
 router = APIRouter()
 
 
 @router.post("/emotion")
-async def get_emotion(url: str):
+async def get_emotion(bucket_name: str, remote_storage_path: str):
     """Get emotions from video and audio."""
-    emotions = get_emotions(url)
-    return emotions
+    return get_emotion_data(bucket_name, remote_storage_path)
