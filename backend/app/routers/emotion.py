653f2dfb2a175bde7d404b8b800f17ebf2190d70
Andrew Hinh <ajhinh@gmail.com>
2023-10-29 09:41:05 -0700

    Added question param to /emotion

--- backend/app/routers/emotion.py
+++ backend/app/routers/emotion.py
@@ -8,10 +8,10 @@ router = APIRouter()
 
 
 @router.post("/emotion")
-def get_emotion(bucket_name: str, remote_storage_path: str):
+def get_emotion(bucket_name: str, remote_storage_path: str, question: str):
     """Get emotions from video and audio."""
     emotion_data = get_emotion_data(bucket_name, remote_storage_path)
-    report = generate_report(emotion_data)
+    report = generate_report(emotion_data, question)
     return {
         "data": emotion_data,
         "report": report,
